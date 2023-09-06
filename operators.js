44916a49a5d5244f03fecb2da453ca78e95cb986
farhad tayebipour <tayebipourf@gmail.com>
2023-09-06 17:59:21 +0330

    java.js

---
+++ operators.js
@@ -0,0 +1,9 @@
+
+// console.log( 10 + 10 )
+// console.log( 10 + 15 )
+// console.log( 10 + 0.5 )
+// console.log( 10 - 5 )
+// console.log( 10 * 10 )
+// console.log( 10 / 2 )
+// console.log( 10000*0.2 )
+console.log( 10 ^ 2 )
---
+++ values.js
@@ -0,0 +1,21 @@
+//number
+10
+15
+30
+
+2.3
+0.2
+1.23
+
+//string
+"this is a test"
+'this is another'
+
+//boolean
+true
+false
+
+//empty values
+null
+undefined
+
